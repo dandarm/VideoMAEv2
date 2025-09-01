5ea967b02b7ebba9e1f7cb8dddd3c6fb51c22574
dandarm <daniele.darmiento@gmail.com>
2025-09-01 22:29:49 +0000

    verify metrics in training

--- engine_for_finetuning.py
+++ engine_for_finetuning.py
@@ -228,6 +228,7 @@ def validation_one_epoch(data_loader, model, device, criterion: Optional[torch.n
 
         preds = output.argmax(dim=1)
         batch_metrics = evaluate_binary_classifier_torch(targets_ondevice, preds, show_report=False)
+        accuracy = batch_metrics["accuracy"]
         balanced_accuracy = batch_metrics["balanced_accuracy"]
         recall = batch_metrics["recall"]
         far = batch_metrics["far"]
@@ -235,6 +236,7 @@ def validation_one_epoch(data_loader, model, device, criterion: Optional[torch.n
 
         batch_size = images.shape[0]
         metric_logger.update(loss=loss.item())
+        metric_logger.meters['acc'].update(accuracy, n=batch_size)
         metric_logger.meters['bal_acc'].update(balanced_accuracy, n=batch_size)
         #metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
         metric_logger.meters['pod'].update(recall, n=batch_size)
