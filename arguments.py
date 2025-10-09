c132b325cf682c975557f80f819d0440bcd03dfe
dandarm <daniele.darmiento@gmail.com>
2025-10-09 11:08:09 +0000

    leonardo

--- arguments.py
+++ arguments.py
@@ -477,7 +477,7 @@ def prepare_tracking_args(machine=None):
         'cloudy': False,
         'use_class_weight': False,
 
-        'epochs': 200,
+        'epochs': 1000,
         'start_epoch_for_saving_best_ckpt': 90,
         'momentum': 0.9,
         'weight_decay': 0.05,
